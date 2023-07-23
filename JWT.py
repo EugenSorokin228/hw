1993b6a255e95996d08a535ab2fe22e612966766
Eugen <odesa.sorokin@gmail.com>
2023-07-23 21:13:40 +0300

    commit

--- JWT.py
+++ JWT.py
@@ -7,5 +7,20 @@ payload = {
     "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 15),
     "iat": datetime.datetime.utcnow()
 }
-encoded_jwt = jwt.encode(payload=payload, key='secret', algorithm="HS256")
-decoded = jwt.decode(encoded_jwt, 'secret', algorithms=['HS256'])
+
+
+def encode_jwt(payload_):
+    encoded_jwt = jwt.encode(payload=payload, key='secret', algorithm="HS256")
+
+    return encoded_jwt
+
+
+def decode_jwt(encoded_jwt_):
+    try:
+        decoded = jwt.decode(encoded_jwt_, 'secre5t', algorithms=['HS256'])
+    except jwt.exceptions.InvalidSignatureError:
+        return False
+    return decoded
+
+
+print(decode_jwt(encode_jwt(payload_=payload)))
